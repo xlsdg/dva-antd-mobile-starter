cd6576e18fd34571cefe2b2306084232c8f23a38
xLsDg <xlsdg@qq.com>
2017-04-26 16:00:29 +0800

    chore(roadhog): change file type

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,30 +1,33 @@
 import pxtorem from 'postcss-pxtorem';
 
 export default {
-  "entry": "src/index.js",
-  "disableCSSModules": false,
-  "publicPath": "/",
-  "autoprefixer": {
-    "browsers": [
-      "iOS >= 8",
-      "Android >= 4"
+  'entry': 'src/index.js',
+  'disableCSSModules': false,
+  'publicPath': '/',
+  'autoprefixer': {
+    'browsers': [
+      'iOS >= 8',
+      'Android >= 4'
     ]
   },
-  "proxy": null,
-  "extraPostCSSPlugins": [
+  'proxy': null,
+  'extraPostCSSPlugins': [
     pxtorem({
       rootValue: 100,
       propWhiteList: [],
     }),
   ],
-  "extraBabelPlugins": [
-    "transform-runtime",
-    ["import", { "libraryName": "antd-mobile", "style": true }]
+  'extraBabelPlugins': [
+    'transform-runtime',
+    ['import', {
+      'libraryName': 'antd-mobile',
+      'style': true
+    }]
   ],
-  "env": {
-    "development": {
-      "extraBabelPlugins": [
-        "dva-hmr"
+  'env': {
+    'development': {
+      'extraBabelPlugins': [
+        'dva-hmr'
       ]
     }
   }
