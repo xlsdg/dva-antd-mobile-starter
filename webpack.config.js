1d3f024f2e133ae9d0985d88d762fbeedcb51ed0
xLsDg <xlsdg@qq.com>
2017-12-08 11:25:26 +0800

    chore(webpack): add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = function(config, env) {
   if (env === 'production') {
     config.entry = {
-      index: './src/index.js',
+      index: ['babel-polyfill', './src/index.js'],
       common: ['react', 'react-dom']
     };
   }
